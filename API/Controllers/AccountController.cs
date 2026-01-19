7b820b18bb837dbf6a1fa4ea0462859b5295318a
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-01-19 15:25:48 +0200

    feat(api): implement user registration with DTO and password hashing

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -11,14 +13,19 @@ public class AccountController(DataContext context) : BaseApiController
 {
 
     [HttpPost("register")]//acount/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExist(registerDto.Username))
+        {
+            return BadRequest("Username is taken");
+        }
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF32.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF32.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -26,5 +33,10 @@ public class AccountController(DataContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExist(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
     }
 }
