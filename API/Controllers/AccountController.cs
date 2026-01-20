6f30b4576355a5c158b149d819caf940a28a15bc
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-01-20 13:08:27 +0200

    feat(api): implement login functionality with password validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,6 +35,29 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]//acount/login
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x =>
+             x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF32.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid password");
+            }
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExist(string username)
     {
         return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
