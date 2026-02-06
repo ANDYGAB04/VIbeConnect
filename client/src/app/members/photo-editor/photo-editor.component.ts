370a53f3da9329716e67c6c2b2f75ec3fa259ab8
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-06 11:47:02 +0200

    feat(photo-editor): enhance photo upload interface and improve loading spinner

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { Member } from '../../_models/member';
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
@@ -8,7 +8,7 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
-  imports: [NgFor,NgIf,NgStyle,NgClass,FileUploadModule],
+  imports: [NgFor,NgIf,NgStyle,NgClass,FileUploadModule,DecimalPipe],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css'
 })
@@ -16,7 +16,7 @@ export class PhotoEditorComponent implements OnInit {
   private accountService = inject(AccountService);
   member = input.required<Member>();
   uploader?:FileUploader;
-  hasBaseDropZoneOver=true;
+  hasBaseDropZoneOver=false;
   baseUrl=environment.apiUrl;
   memberChange=output<Member>();
 
