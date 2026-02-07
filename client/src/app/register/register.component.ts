ca6f7a9dd13ca030a611a283fef6ad6f28618fb6
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-07 18:05:25 +0200

    feat(register): fix knownAs field name and add validation error handling in registration form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,26 +1,26 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { ToastrService } from 'ngx-toastr';
 import { JsonPipe, NgIf } from '@angular/common';
 import { TextInputComponent } from '../_forms/text-input/text-input.component';
 import { DatePickerComponent } from '../_forms/date-picker/date-picker.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, DatePickerComponent],
+  imports: [ReactiveFormsModule, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
   private acccountService = inject(AccountService);
+  private router=inject(Router);
   private fb=inject(FormBuilder);
-  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
-  model: any = {};
   registerForm:FormGroup = new FormGroup({});
   maxDate=new Date();
+  validationErrors:string[] | undefined;
 
   ngOnInit(): void {
     this.intializeForm();
@@ -31,7 +31,7 @@ export class RegisterComponent implements OnInit {
     this.registerForm=this.fb.group({
       gender:['male'],
       username : ['', Validators.required],
-      knowAs:['',Validators.required],
+      knownAs:['',Validators.required],
       dateOfBirth:['',Validators.required],
       city:['',Validators.required],
       country:['',Validators.required],
@@ -50,19 +50,24 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value)
-    // this.acccountService.register(this.model).subscribe({
-    //   next: (response) => {
-    //     console.log(response);
-    //     this.cancel();
-    //   },
-    //   error: (error) => {
-    //     this.toastr.error(error.error);
-    //   },
-    // });
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.patchValue({dateOfBirth:dob});
+    this.acccountService.register(this.registerForm.value).subscribe({
+      next: (response) => this.router.navigateByUrl('/members'),
+      error: (error) => {
+        this.validationErrors=error
+      },
+    });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
   }
+
+  private getDateOnly(dob:string | undefined){
+    if(!dob) return;
+    return new Date(dob).toISOString().slice(0,10);
+  }
 }
+
+
