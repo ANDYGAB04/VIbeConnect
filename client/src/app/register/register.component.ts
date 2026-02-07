de53756114d2865b3166199b4f82efdf1d530c1b
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-07 12:10:21 +0200

    feat(register): migrate to reactive forms and update registration logic

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,32 +1,45 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private acccountService = inject(AccountService);
   private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
+  registerForm:FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.intializeForm();
+  }
+
+  intializeForm(){
+    this.registerForm=new FormGroup({
+      username : new FormControl(),
+      password: new FormControl(),
+      confirmPassword:new FormControl(),
+    })
+  }
 
   register() {
-    this.acccountService.register(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => {
-        this.toastr.error(error.error);
-      },
-    });
+    console.log(this.registerForm.value)
+    // this.acccountService.register(this.model).subscribe({
+    //   next: (response) => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: (error) => {
+    //     this.toastr.error(error.error);
+    //   },
+    // });
   }
 
   cancel() {
