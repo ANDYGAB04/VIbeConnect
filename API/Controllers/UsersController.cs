b1aab6a269b101689d57cc65f19e2570546cbfba
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-09 13:55:03 +0200

    feat(pagination): add pagination header and user parameters classes

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -23,7 +23,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
         return Ok(users);
     }
 
-    [HttpGet("{username}")] // api/users/id
+    [HttpGet("{username}")] // api/users/username
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         var user = await userRepository.GetMemberAsync(username);
