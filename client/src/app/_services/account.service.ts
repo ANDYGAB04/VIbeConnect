9664a0a2e5ba26c3bb857b6f606fe3521c4fed7c
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-18 13:29:48 +0200

    feat(admin): add admin panel with guard and navigation link

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -10,9 +10,17 @@ import { LikesService } from './likes.service';
 })
 export class AccountService {
   private http = inject(HttpClient);
-  private likeService=inject(LikesService);
+  private likeService = inject(LikesService);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
+  roles = computed(() => {
+    const user = this.currentUser();
+    if (user && user.token) {
+      const role = JSON.parse(atob(user.token.split('.')[1])).role;
+      return Array.isArray(role) ? role : [role];
+    }
+    return [];
+  })
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
@@ -34,7 +42,7 @@ export class AccountService {
       }),
     );
   }
-  setCurentUser(user:User){
+  setCurentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
     this.likeService.getLikeIds();
