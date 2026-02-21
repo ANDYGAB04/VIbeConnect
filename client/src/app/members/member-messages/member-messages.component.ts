904b8dbc91f75e8da718b0e56c0e7bc5790f78e5
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-02-21 12:05:10 +0200

    feat(signalr): implement SignalR hub connection in MessageService and update MemberDetailComponent for real-time messaging

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,29 +1,25 @@
-import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
-import { Message } from '../../_models/message';
+import { Component, inject, input, ViewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-member-messages',
   standalone: true,
-  imports: [TimeagoModule,FormsModule],
+  imports: [TimeagoModule, FormsModule],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css'
 })
 export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
-  private messageService = inject(MessageService)
-   username = input.required<string>();
-   messages = input.required<Message[]>();
-   messageContent='';
-   updateMessages=output<Message>();
-   
-   sendMessage(){
-    this.messageService.sendMessage(this.username(),this.messageContent).subscribe({
-      next : message =>{
-          this.updateMessages.emit(message);
-          this.messageForm?.reset();
+  messageService = inject(MessageService)
+  username = input.required<string>();
+  messageContent = '';
+
+  sendMessage() {
+    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
+      next: message => {
+        this.messageForm?.reset();
       }
     })
-   }
+  }
 }
